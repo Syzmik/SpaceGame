a894fee12a86b3aef9acc4c1c321c0e9ce682037
Michael Burress <Syzmik117@gmail.com>
2022-12-05 08:04:25 -0500

    name change

--- game.py
+++ game.py
@@ -7,14 +7,14 @@ pygame.init()
 
 win = pygame.display.set_mode((500,500))
 
-pygame.display.set_caption("Space Dink")
+pygame.display.set_caption("POVERTY Online")
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 bg = pygame.image.load('earth5001.png')
 char = pygame.image.load('standing.png')
 Lectrumimg = pygame.image.load('Lectrum.png')
-chrolastinimg = pygame.image.load('Chrolastin.png')
+chrolastinimg = pygame.image.load('Chrolastin.png')0
 eleriumimg = pygame.image.load('elerium.png')
 terrilliumimg = pygame.image.load('terillium.png')
 clock = pygame.time.Clock()
